ac5127a163144550740a562a34b28d0df7907a48
chenIshi <chen.ishi@gmail.com>
2018-02-12 16:20:53 +0800

    first proto

--- js/app.js
+++ js/app.js
@@ -20,41 +20,57 @@ $(document).ready(function () {
 
 
 
-
-var kang_content = $("<b>Hello world 0!</b>")
-var cheng_content = $("<b>Hello world 1!</b>")
-var future_content = $("<b>Hello world 2!</b>")
-var string3 = $("<b>Hello world 3!</b>")
-var default_content = $("<b>Hello world d!</b>")
+var kang_title = $("<h2>Kang Ming Senior High</h2>")
+var cheng_title = $("<h2>National Cheng Kung University</h2>")
+var future_title = $("<h2>Future......</h2>")
+var third_title = $("<h2>Kang Ming Senior High</h2>")
+var default_title = $("<h2>Page Not Constructed !!</h2>")
+var kang_content = $("<p>Hello world 0!</p>")
+var cheng_content = $("<p>Hello world 1!</p>")
+var future_content = $("<p>Hello world 2!</p>")
+var string3 = $("<p>Hello world 3!</p>")
+var default_content = $("<p>Hello world d!</p>")
 
 
 
 function switch_time_section(){
 
-    $("#senior_high, #college, #future, #tag3, #default").remove();
+  edu_btn_times ++;
+
+    $("#senior_high_content, #college_content, #future_content, #tag3, #default_content").remove();
+    $("#senior_high_title, #college_title, #future_title, #tag3, #default_title").remove();
     switch(edu_btn_times%edu_time_section_num){
         case 0:
+        //exp-title change
+          $(".edu-title").append(kang_title);
+          kang_title.attr('id','senior_high_title');
+        //exp-content change
           $(".edu-exp").append(kang_content);
-          kang_content.attr('id', 'senior_high');
+          kang_content.attr('id', 'senior_high_content');
           break;
         case 1:
+          $(".edu-title").append(cheng_title);
+          cheng_title.attr('id','college_title');        
           $(".edu-exp").append(cheng_content);
-          cheng_content.attr('id', 'college');
+          cheng_content.attr('id', 'college_content');
           break;
         case 2:
+          $(".edu-title").append(future_title);
+          future_title.attr('id','future_title');
           $(".edu-exp").append(future_content);
-          future_content.attr('id', 'future');
+          future_content.attr('id', 'future_content');
           break;
         case 3:
           $("#edu-exp").append(string3);
           string3.attr('id', 'tag3');
           break;
         default:
+          $(".edu-title").append(default_title);
+          default_title.attr('id','default_title');
           $("#edu-exp").append(default_content);
-          default_content.attr('id', 'default');
+          default_content.attr('id', 'default_content');
           break;
     }
-    edu_btn_times ++;
 }
 
 
